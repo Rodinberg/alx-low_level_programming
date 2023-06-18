7ecda491e84fd84b6f510e8c9959be80f14ee1e1
Rodinberg <rodinbergappiah08@gmail.com>
2023-06-18 05:34:33 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - main entry point
+ *return: alway 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
