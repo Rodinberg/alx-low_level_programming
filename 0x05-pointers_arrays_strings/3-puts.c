7772bd5eec03f07242048c98db8d6c94878ee5f3
Rodinberg <rodinbergappiah08@gmail.com>
2023-06-26 22:04:59 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string
+ * @str: The string to print
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+	}
+	putchar('\n');
+}
