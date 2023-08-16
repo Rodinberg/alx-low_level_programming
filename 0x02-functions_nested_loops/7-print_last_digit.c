29ac0f57e6504b44d5738d3b8db33f40b1085921
Rodinberg <rodinbergappiah08@gmail.com>
2023-08-16 13:40:21 -0700

    7-last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,17 +7,13 @@
  */
 int print_last_digit(int n)
 {
-	int a;
+	int last;
 
-	if (n < 0)
+	last = n % 10;
+	if (last < 0)
 	{
-		n = -n;
-		a = n % 10;
+		last = last * -1;
 	}
-	if (a < 0)
-	{
-		a = -a;
-	}
-	_putchar(a * '0');
-	return (a);
+	_putchar(last * '0');
+	return (last);
 }
